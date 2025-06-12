1429be8220356e57d695f3cb0edfeebe02ae1fc5
justintjs12 <justintjs12@gmail.com>
2025-06-12 22:25:37 +0100

    Add blog creation and contact features with UI enhancements
    Introduces functionality for creating blog articles, including form handling and database interaction. Adds a contact page with a form for user inquiries. Updates navigation links and enhances the homepage with an improved layout and styling. Includes new Twig templates for the create article and contact pages. Enhances form theming with Bootstrap 5 integration.

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,8 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\MyArticles;
-
-
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 
 final class BlogController extends AbstractController
@@ -25,19 +28,51 @@ final class BlogController extends AbstractController
     }
 
     #[Route('/', name: 'home')]
-
     public function home(): Response
     {
         return $this->render('blog/home.html.twig', [
-            'title' => 'welcome to you !',
-            'content' => 'notre belle application '
+            'title' => 'Bienvenue sur notre blog ! !'
         ]);
     }
-    #[Route('/blog/{id}', name: 'blog_show')]
-    public function showArticle(MyArticles $article): Response
+
+    #[Route('/blog/create', name: 'blog_create')]
+    public function createArticle(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $article = new MyArticles();
+        $formArticles = $this->createFormBuilder($article)
+            ->add('title', TextType::class, ['label' => 'Titre'])
+            ->add('content', TextareaType::class, ['label' => 'Contenu'])
+            ->add('image', TextType::class, ['label' => 'Image'])
+            ->getForm();
+
+        $formArticles->handleRequest($request);
+
+        if ($formArticles->isSubmitted() && $formArticles->isValid()) {
+            $article->setCreatedAt(new \DateTimeImmutable());
+            $entityManager->persist($article);
+            $entityManager->flush();
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/createArticle.html.twig', [
+            'form' => $formArticles->createView(),
+        ]);
+    }
+
+    #[Route('/blog/contact', name: 'blogContact')]
+    public function contactBlog()
+    {
+        return $this->render('blog/contactBlog.html.twig', [
+            'title' => 'Contact',
+            'content' => 'Contactez-nous pour plus d\'informations.',
+        ]);
+    }
+
+    #[Route('/blog/{id}', name: 'blog_show', requirements: ['id' => '\d+'])]
+    public function showArticle(\App\Entity\MyArticles  $MyArticles ): Response
     {
         return $this->render('blog/showArticle.html.twig', [
-            'article' => $article,
+            'article' => $MyArticles,
         ]);
     }
 }
